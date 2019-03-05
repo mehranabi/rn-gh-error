4c7b262399c4ed2d1ac893fcb7941be68ca8a4dd
Mehran Abghari <mehran.ab80@gmail.com>
2019-03-05 23:26:16 +0330

    Add MOCK

--- __tests__/App.js
+++ __tests__/App.js
@@ -10,6 +10,26 @@ import App from '../src/App'
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 
+jest.mock('NativeModules', () => ({
+  UIManager: {
+    RCTView: () => ({
+      directEventTypes: {},
+    }),
+  },
+  KeyboardObserver: {},
+  RNGestureHandlerModule: {
+    attachGestureHandler: jest.fn(),
+    createGestureHandler: jest.fn(),
+    dropGestureHandler: jest.fn(),
+    updateGestureHandler: jest.fn(),
+    State: {},
+    Directions: {},
+  },
+  PlatformConstants: {
+    forceTouchAvailable: false,
+  },
+}))
+
 it('renders correctly', () => {
   renderer.create(<App />)
 })
